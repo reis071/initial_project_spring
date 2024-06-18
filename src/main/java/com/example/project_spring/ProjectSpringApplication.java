7586154f13c5f3294f1b3353a667f0c2a04f67a7
reis071 <reiscorreia071@gmail.com>
2024-06-18 09:47:45 -0300

    start project spring

---
+++ src/main/java/com/example/project_spring/ProjectSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.project_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringApplication.class, args);
+    }
+
+}
